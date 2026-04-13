02d0c10ab4a3db78cf86ed505bfbff83d94d4a84
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-13 18:02:59 -0300

    refactor: changed list to a dictionary

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+restaurantes = [{'nome': 'Praça', 'categoria': 'Japonesa', 'ativo': False}, 
+                {'nome':'Pizza Suprema', 'categoria': 'Pizza', 'ativo': True},
+                {'nome': 'Cantina', 'categoria': 'Italiana', 'ativo': False}]
+
 
 def exibir_nome_do_programa():
     print("""
@@ -53,7 +56,11 @@ def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
 
     for restaurante in restaurantes:
-        print(f'. {restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+
+        print(f' - {nome_restaurante} | {categoria} | {ativo}')
 
     voltar_ao_menu_principal()
 
