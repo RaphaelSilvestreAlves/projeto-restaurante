7a76263c7166e1b1a91e50bdc64f245aa38a4cfb
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-15 15:21:21 -0300

    add: added docstring to functions

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ restaurantes = [{'nome': 'Praça', 'categoria': 'Japonesa', 'ativo': False},
 
 
 def exibir_nome_do_programa():
+    '''Essa função é responsável por mostrar o título no console para o usuário'''
     print("""
       
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░
@@ -24,16 +25,19 @@ def exibir_nome_do_programa():
       """)
 
 def exibir_opcoes():
+    '''Essa função é responsável por mostrar as opções no console para o usuário'''
     print('1. Cadastrar Restaurante')
     print('2. Listar Restaurante')
     print('3. Alternar estado do Restaurante')
     print('4. Sair\n')
 
 def opcao_invalida():
+    '''Essa função é responsável de mostrar um erro ao usuário e retorná=lo ao menu'''
     print('Opção Inválida!\n')
     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
+    '''Essa função é responsável por limpar o console e mostrar o subtitulo definido na função no console'''
     os.system('cls') #windows
     #os.system('clear') MAC
     linha = '*' * (len(texto))
@@ -43,10 +47,21 @@ def exibir_subtitulo(texto):
     print()
 
 def voltar_ao_menu_principal():
+    '''Essa função é responsável por realizar o retorno do usuário ao menu principal
+    digitando qualquer tecla'''
     input('\nDigite uma tecla para voltar ao menu principal')
     main()
 
 def cadastrar_restaurante():
+    '''Essa função é responsável por cadastrar um novo restaurante
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Output:
+    - Adiciona um novo restaurante a lista de restaurantes
+    '''
     exibir_subtitulo("Cadastro de novos restaurantes")
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
@@ -57,6 +72,7 @@ def cadastrar_restaurante():
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
+    '''Essa função é responsável por listar os restaurantes no console para o usuário'''
     os.system('cls')
     exibir_subtitulo('Listando restaurantes')
 
@@ -71,6 +87,7 @@ def listar_restaurantes():
     voltar_ao_menu_principal()
 
 def alternar_estado_restaurante():
+    '''Essa função é responsável por alternar o estado dos restaurantes no console para o usuário'''
     exibir_subtitulo('Alternando estado do restaurante')
     nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
     restaurante_encontrado = False
@@ -85,6 +102,7 @@ def alternar_estado_restaurante():
     voltar_ao_menu_principal()
 
 def escolher_opcao():
+    '''Essa função é responsável por encaminhar o usuário conforme a opção escolhida'''
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
 
@@ -102,10 +120,12 @@ def escolher_opcao():
         opcao_invalida()
 
 def finalizar_app():
+    '''Essa função é responsável por mostrar a mensagem de encerramento'''
     exibir_subtitulo('Finalizando programa')
 
 
 def main():
+    '''Essa função é responsável por inicializar o programa chamando outras funções'''
     os.system('cls') # windows
     exibir_nome_do_programa()
     exibir_opcoes()
